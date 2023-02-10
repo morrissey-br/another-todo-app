3bdb0af524b9a7f133c5c171021a03d7ed902ece
Samuel Morrissey <samuel.morrissey@outlook.com>
2023-02-10 09:48:10 -0300

    Início dos estudos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,29 @@
-import './globals.css'
+import "./global.css";
+import { FaBars } from "react-icons/fa";
 
-export default function RootLayout({
+export default function GlobalLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header className="flex justify-between p-4 items-center mb-2">
+          <h1 className="text-xl font-mono">Another Todo App</h1>
+          <nav className="flex gap-4 font-mono">
+            <button className="rounded-xl p-2 hover:bg-black hover:text-white hover:transition-colors md:hidden">
+              <FaBars className="text-lg" />
+            </button>
+            <a className="self-center hidden md:inline-block">Sing in</a>
+            <button className="border-2 border-black rounded-xl p-2 hover:bg-black hover:text-white hover:transition-colors hidden md:inline-block">
+              Sing up
+            </button>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
